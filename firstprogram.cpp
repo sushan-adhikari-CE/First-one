49f9d83e98a2e9a0c55befefeed36500ed860260
Sushan Adhikari <sa06021321@student.ku.edu.np>
2023-03-10 10:02:29 +0545

    Fixed some bugs

--- firstprogram.cpp
+++ firstprogram.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     std::cout << "This is my first code in GitHub" << std::endl;
-    std::cout << "First change I have made";<<std::endl;
-    std::cout<<"I added it on github so you can pull";
+    std::cout << "First change I have made" << std::endl;
+    std::cout << "I added it on github so you can pull";
     return 0;
 }
