b6f8084ad428a821362d06bedce8e7151f3a9824
Sushan Adhikari <sa06021321@student.ku.edu.np>
2023-03-10 10:21:32 +0545

    Solved the merge conflict

--- firstprogram.cpp
+++ firstprogram.cpp
@@ -3,6 +3,7 @@ int main()
 {
     std::cout << "This is my first code in GitHub" << std::endl;
     std::cout << "First change I have made" << std::endl;
-    std::cout << "let's see conflict I added it on github so you can pull and resolve it";
+    std::cout << "let's see conflict I added it on github so you can pull and resolve it" << std::endl;
+    std::cout << "Resolved the conflict";
     return 0;
 }
